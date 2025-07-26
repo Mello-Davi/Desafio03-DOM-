a960a4b20968ef2601ae5e8df3b91613feb8b22f
Mello-Davi <davirmelo16@gmail.com>
2025-07-25 22:50:04 -0300

    feat: box shadow na calculadora e inicio do dom

--- script_dom.js
+++ script_dom.js
@@ -1,5 +1,12 @@
-//
+//inicialização das variáveis
+let display = document.querySelector("#displayDasNotas");
+let btn_adicionar = document.querySelector("#btn_adicionar");
+let btn_resultado = document.querySelector("#btn_resultado");
+let input = document.querySelector("#inputNotas");
 
+//função para colocar a nota dentro do display
+function adicionar_nota(){
+    
 
-//função de receber mensagem e appendar na caixa #displayDasNotas 
+}
 
